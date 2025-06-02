c47086fd4fb8424aaf4d6735bec62291b3ad9e54
huangxuan <3441578327@qq.com>
2025-06-02 19:27:51 +0800

    fix(utils): 优化 SimpleRedisLock 实现
    - 添加线程标识，确保只有加锁线程可以解锁
    - 使用 UUID 前缀区分不同实例的线程标识
    - 修改 tryLock 方法参数名，提高代码可读性
    - 完善 unlock 方法，增加线程标识检查

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,7 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.IdUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -16,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 public class SimpleRedisLock {
     private StringRedisTemplate stringRedisTemplate;
     private String name;
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -24,14 +27,24 @@ public class SimpleRedisLock {
 
     public static final String KEY_PREFIX = "lock:";
 
-    public boolean tryLock(Long timeSec) {
-        //获取锁
+    public boolean tryLock(Long timeoutSec) {
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, "1", timeSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标示
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标示是否一致
+        if(threadId.equals(id)) {
+            // 释放锁
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
